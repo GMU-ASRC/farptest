14836c3a085aefa0a463ccb3407d3636f7e2af0e
kenblu24 <kenblu24@hotmail.com>
2026-07-05 22:00:38 -0400

    allow passing tqdm kwargs

--- eval_genome.py
+++ eval_genome.py
@@ -41,7 +41,7 @@ def fitness_single(
     return stat, out
 
 
-def test_genome_mp(genome, n=6, rng_seed=20, trials=100):
+def test_genome_mp(genome, n=6, rng_seed=20, trials=100, tqdm_kwargs={}):
     seeds = np.random.default_rng(rng_seed).integers(
         0, 2**31, size=trials, dtype=np.int64
     )
@@ -57,7 +57,7 @@ def test_genome_mp(genome, n=6, rng_seed=20, trials=100):
         )
         for seed in seeds
     ]
-    ret_arr = process_map(fitness_single, configs)
+    ret_arr = process_map(fitness_single, configs, **tqdm_kwargs)
     stats, successes = zip(*ret_arr)
 
     rate = 1 - sum(successes) / len(seeds)
