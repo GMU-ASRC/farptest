8c578e3ff1e21ea30cc66b4733e199196a676b58
michabay05 <michabay05@gmail.com>
2026-07-31 14:13:24 -0400

    fix: pass through constructor arguments

--- DiffuseSentinel.py
+++ DiffuseSentinel.py
@@ -10,7 +10,7 @@ HALF_ANGLE = 1.4 # min 0.6, max 2
 
 class DiffuseSentinelController(SentinelController):
     def __init__(self, speed_limit=SPEED_LIMIT, turn_limit=TURN_LIMIT, agent=None, parent=None):
-        super().__init__(agent, parent, speed_limit=SPEED_LIMIT, turn_limit=TURN_LIMIT)
+        super().__init__(agent, parent, speed_limit=speed_limit, turn_limit=turn_limit)
         self.pseudoangle = 0 # track the agent angle internally
         self.pseudofirstseen = 0 # track the step at which a defender is seen, this is to stop the overlap prevention system from getting stuck
 
