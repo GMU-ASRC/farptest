0de5f616a24f3760e1db9a4fc717568d9a32bf58
jhudd135 <jhudd135@gmail.com>
2026-07-31 16:20:27 -0400

    we moving different they cant touch me apf evader

--- CustomEvader.py
+++ CustomEvader.py
@@ -1,24 +1,82 @@
-from swarmsim.agent.control.WaypointPIDController import WaypointPIDController, DEFAULT_PID_ARGS
-from swarmsim.sensors.BinaryFOVSensor import BinaryFOVSensor
+from swarmsim.world.RectangularWorld import RectangularWorld
+from swarmsim.agent.MazeAgent import MazeAgent
+from swarmsim.sensors.BinaryFOVSensor import BinaryFOVSensor, vectorize, turn, project, lineCircleIntersect
+from swarmsim.agent.control.AbstractController import AbstractController
+import pygame
+import numpy as np
 
+SPEED_LIMIT = 0.3
+TURN_LIMIT = 0.6
 
-class CustomEvader(WaypointPIDController):
-    def __init__(
-        self,
-        agent=None, parent=None,
-        targeter_sensor_id=None,
-        speed_pid=DEFAULT_PID_ARGS,
-        steer_pid=DEFAULT_PID_ARGS,
-        static_waypoint=None,
-        **kwargs
-    ):
-        super().__init__(agent=agent, parent=parent,
-                         sensor_id=targeter_sensor_id,
-                         speed_pid=speed_pid, steer_pid=steer_pid,
-                         static_waypoint=static_waypoint,
-                         **kwargs)
+def smallest_angular_difference(a1, a2):
+    a = a1 - a2
+    return (a + np.pi) % (2*np.pi) - np.pi
 
-    def get_actions(self, agent):
-        binary: BinaryFOVSensor = agent.sensors[1]
-        v, w = (0, 0) if binary.current_state else super().get_actions(agent)
-        return v, w
+class CustomEvader(AbstractController):
+    def draw(self, screen, offset=((0, 0), 1.0)):
+        pan, zoom = np.asarray(offset[0]), np.asarray(offset[1])
+
+        sight_color = (200, 100, 0)
+
+        for defender in [a for a in self.agent.world.population if a.team == "blue"]:
+            bfovs: BinaryFOVSensor = defender.sensors[1]
+            vec = self.get_nearest_point_of_sensor(bfovs)
+            head = self.agent.position * zoom + pan
+            tail = (self.agent.position + vec) * zoom + pan
+            pygame.draw.line(screen, sight_color, head, tail)
+    
+    def __init__(self, agent=None, parent=None, **kwargs):
+        super().__init__(agent, parent)
+
+    def point_normal_to_segment(self, segvec, point):
+        orthovec = np.array([segvec[1], -segvec[0]])
+        return np.sign(turn(orthovec, segvec - point)) != np.sign(turn(orthovec, -point))
+
+    def get_nearest_point_of_sensor(self, sensor: BinaryFOVSensor):
+        options = []
+        pos = self.agent.position
+        agent: MazeAgent = sensor.agent
+        options.append(agent.position - pos) # cone point / sensor origin
+        left_rel, right_rel = sensor.getSectorVectors()
+        left_rel, right_rel = sensor.r * np.asarray(left_rel[:2]), sensor.r * np.asarray(right_rel[:2])
+        left_abs, right_abs = left_rel + agent.position, right_rel + agent.position
+        options.append(left_abs - pos) # left whisker end
+        options.append(right_abs - pos) # right whisker end
+        left_ortho, right_ortho = np.array([left_rel[1], -left_rel[0]]), np.array([right_rel[1], -right_rel[0]])
+        if self.point_normal_to_segment(left_rel, pos - agent.position):
+            options.append(project(left_abs - pos, left_ortho)) # if applicable, intermediate point on left whisker
+        if self.point_normal_to_segment(right_rel, pos - agent.position):
+            options.append(project(right_abs - pos, right_ortho)) # if applicable, intermediate point on right whisker
+        
+        l180 = sensor.theta * 2 < np.pi
+        u = pos - agent.position  # vector to agent
+        leftTurn = turn(u, left_rel)
+        rightTurn = turn(u, right_rel)
+        # if fov < 180 use between minor arc, otherwise use not between minor arc
+        if rightTurn <= 0 and 0 <= leftTurn if l180 else not (leftTurn < 0 and 0 < rightTurn):
+            options.append(agent.position - pos + (sensor.r / np.linalg.norm(agent.position - pos)) * (pos - agent.position))
+        return min(options, key=lambda v : np.sum(np.square(v)))
+
+    def get_actions(self, agent: MazeAgent):
+        pos = agent.position
+        vector_sum = np.array([0, 0], dtype=np.float64)
+        for defender in [a for a in agent.world.population if a.team == "blue"]:
+            bfovs: BinaryFOVSensor = defender.sensors[1]
+            vec = self.get_nearest_point_of_sensor(bfovs)
+            mag = np.linalg.norm(vec)
+            vector_sum += (-1 / mag**2) / mag * vec
+        
+        goal = agent.world.population[0]
+        gvec = goal.position - pos
+        vector_sum += 10 * (gvec / np.linalg.norm(gvec))
+
+        angle = np.atan2(vector_sum[1], vector_sum[0])
+        sad = smallest_angular_difference(agent.angle, angle)
+        
+        self.view_angle = angle
+        self.view_magnitude = np.linalg.norm(vector_sum)
+        if abs(sad) < np.pi / 2:
+            v, w = SPEED_LIMIT, -np.sign(sad)
+        else:
+            v, w = -SPEED_LIMIT, -np.sign(sad)
+        return np.clip(v, -SPEED_LIMIT, SPEED_LIMIT), np.clip(w, -TURN_LIMIT, TURN_LIMIT)  # DO NOT CHANGE THIS LINE
