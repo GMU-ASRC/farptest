fc09998bcf6b0289c70f4ef5710340e9af7b9ce0
jhudd135 <jhudd135@gmail.com>
2026-07-31 21:14:15 -0400

    99% amazing defense using goal sensor and 2 stages now that BinaryFOVSensor is fixed

--- CustomController.py
+++ CustomController.py
@@ -1,24 +1,22 @@
-from swarmsim.agent.control.AbstractController import AbstractController
-from swarmsim.agent.control.MultibitBinaryController import MultibitBinaryController
-from Sentinel import setinel_draw
+from Sentinel import setinel_draw, SentinelController
 import numpy as np
 
 SPEED_LIMIT = 0.3
 TURN_LIMIT = 0.6
 
 
-class CustomController(MultibitBinaryController):
-    def draw(self, screen, offset=((0, 0), 1.0)):
-        setinel_draw(self, screen, offset)
-
+class CustomController(SentinelController):
     def __init__(self, agent=None, parent=None, ):
-        super().__init__({
-            0b00: (SPEED_LIMIT, TURN_LIMIT),
-            0b01: (SPEED_LIMIT, -TURN_LIMIT),
-            0b10: (SPEED_LIMIT, -TURN_LIMIT),
-            0b11: (SPEED_LIMIT, -TURN_LIMIT)
-        }, agent=agent, parent=parent, sensor_ids=[0, 2])
+        super().__init__(agent=agent, parent=parent)
+        self.stage = 1
 
-    def get_actions(self, agent):
-        v, w = super().get_actions(agent)
-        return np.clip(v, -SPEED_LIMIT, SPEED_LIMIT), np.clip(w, -TURN_LIMIT, TURN_LIMIT)  # DO NOT CHANGE THIS LINE
+    def get_v_w(self, detected):
+        if self.stage == 1:
+            v, w = SPEED_LIMIT, -TURN_LIMIT/2
+            if 30 < self.pseudostep and not self.agent.sensors[2].current_state:
+                self.stage = 2
+        else:
+            
+            v = 0 if detected else SPEED_LIMIT
+            w = TURN_LIMIT / 2 if self.agent.sensors[2].current_state else -TURN_LIMIT
+        return v, w
