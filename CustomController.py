abb284803ab65f0c7a1a626a38fa6d320d16f88a
michabay05 <michabay05@gmail.com>
2026-08-07 08:42:28 -0400

    feat: added (pseudo) time step to nn input

--- CustomController.py
+++ CustomController.py
@@ -10,13 +10,13 @@ TURN_LIMIT = 0.6
 VMAX, WMAX = SPEED_LIMIT, TURN_LIMIT
 MAX_BOUND = torch.Tensor((VMAX, WMAX))
 MIN_BOUND = -MAX_BOUND
-INPUT_COUNT = 3
+SENS_WINDOW = 3
 
 
 class FarpRNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.rnn = nn.RNN(input_size=INPUT_COUNT, hidden_size=4, num_layers=1, batch_first=True)
+        self.rnn = nn.RNN(input_size=SENS_WINDOW, hidden_size=4, num_layers=1, batch_first=True)
         self.linear = nn.Linear(in_features=4, out_features=2)
 
     def forward(self, x, h0=None):
@@ -29,18 +29,21 @@ class CustomController(AbstractController):
     def __init__(self, genome=None, sensor_id=0, agent=None, parent=None):
         super().__init__(agent=agent, parent=parent)
         self.sensor_id = sensor_id
-        self.sens_hist = [0] * INPUT_COUNT
+        self.pseudo_step = 0
+        self.sens_hist = [0.0] * SENS_WINDOW
         self.model = FarpRNN()
 
         if genome is None:
-            g = torch.tensor([  3.6984,  10.4825,  -6.1933,  -4.0966,  -0.6312,  -7.0232, -14.4090,
-         -1.6235,   2.2066,  -0.7314,   2.7778,   0.6396, -10.4244,   2.8760,
-          5.3073,  -0.8869,  -4.8985,   5.0579,  -1.5696,  -1.0510,  -3.6404,
-          1.6608,   5.8362,  -3.5595,  -1.3666,  -0.0415,   1.9167,  -3.1388,
-          9.8562,   1.2687,  14.7881,   3.2982,   8.8745,  -7.4220,  -2.0406,
-          0.5301,  -3.0271,   1.4108,   5.7679,  -5.4854,  -8.6641,  -2.8596,
-         -5.0980,  -9.9118,   7.6231,   3.1581])
-            # raise NotImplementedError()
+            # g = torch.tensor([
+            #      0.9843,  -0.0630,  -0.8113,   6.0496,   6.2267,   1.1070,  -0.0827,
+            #     -0.0703,   0.2808,   6.0814,  -2.7982,  -4.3426,  -1.8812,   1.9555,
+            #      3.8920,  -0.1906,   2.3481,  -2.3552,   3.3442,  -8.4039,   1.5242,
+            #     -2.5292,  -4.9531,  -1.6374,  -4.0107,  -2.3715,   2.1547,  -1.4259,
+            #     -3.2585,  -4.7903, -10.0326,  -2.2905,  -2.3988,   1.3420,   5.2107,
+            #     -0.2031,   3.9850,  -3.8695,   0.1669,  -2.9956,   2.0178,   0.1494,
+            #      2.6730,  -2.4851,   3.7443,  -3.8775
+            # ])
+            raise NotImplementedError()
         else:
             if isinstance(genome[0], str):
                 g = torch.from_numpy(np.fromstring(genome[0], sep=' ')).float()
@@ -53,9 +56,10 @@ class CustomController(AbstractController):
     def get_actions(self, agent): # type: ignore
         detected = float(agent.sensors[self.sensor_id].current_state != 0)
         output, _ = self.model(
-            torch.tensor(self.sens_hist[-INPUT_COUNT:]).float().view(1, 1, INPUT_COUNT)
+            torch.tensor([*self.sens_hist[-SENS_WINDOW:], self.pseudo_step]).float().view(1, 1, 1+SENS_WINDOW)
         )
         self.sens_hist.append(detected)
+        self.pseudo_step += 1
 
         return torch.clamp(output, MIN_BOUND, MAX_BOUND).numpy().reshape(2)
 
